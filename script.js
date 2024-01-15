b613c93dda799ecd37dd9cb9e9629a89557babbf
sweana <sweana@bititude.com>
2024-01-15 15:04:06 +0530

    Add javascript promise using async and await

--- script.js
+++ script.js
@@ -54,3 +54,29 @@ washDress().then(message=>{console.log(message); return cleanRoom()})
            .then(message=>{console.log(message); return eatBreakfast()})
            .then(message=>{console.log(message); return console.log("completed all chores")})
            .catch(error=>{console.log(error)})
+
+
+function getProduct(a, b) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(a * b);
+    }, 6000);
+  });
+}
+
+
+
+const printResult = async()=>{
+    try{
+        const result=await getProduct(4,3);
+        const secondResult=await getProduct(2,result);
+        console.log('the final result is',secondResult);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+printResult();
+
+
